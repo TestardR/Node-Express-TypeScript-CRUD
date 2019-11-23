2f0ebe5c1a7c56d5f3246ad5eb440b5370191be2
unknown <rmtestard@gmail.com>
2019-11-23 17:03:46 +0100

    1st commit

---
+++ src/routes/todos.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+  createTodo,
+  getTodos,
+  updatedTodo,
+  deleteTodo
+} from "../controllers/todos";
+
+const router = Router();
+
+router.post("/", createTodo);
+
+router.get("/", getTodos);
+
+router.patch("/:id", updatedTodo);
+
+router.delete("/:id", deleteTodo);
+
+export default router;
